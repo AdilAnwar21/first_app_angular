3c91b5519a977f8ed7b2bc0296d2464942a39e56
Adil <anwaradil295@gmail.com>
2024-03-04 12:42:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+//importing a newly created component
+import { ServerComponent } from './server/server.component';
+import { PageComponent } from './page/page.component';
+import { WarningAlert } from './warning/warning.component';
+import { SuccessAlert } from './success/success.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    PageComponent,
+    WarningAlert,
+    SuccessAlert
   ],
   imports: [
     BrowserModule
